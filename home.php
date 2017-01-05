fc4671b6a150ab557a13aae3e7830abc23eb3ae8
Taylor Richards <taylor.richards@duke.edu>
2017-01-05 16:27:01 -0500

    crazy styling

--- home.php
+++ home.php
@@ -102,7 +102,7 @@
     }
 
     input[type=checkbox]{
-      -webkit-appearance: none;
+      -webkit-appearance: checkbox-container;
       background: #737CA1;
       background-color: #737CA1;
       color: #737CA1;
