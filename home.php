adf8fad21695033db0bfc218dbb3c49d7ce508bc
Taylor Richards <taylor.richards@duke.edu>
2017-01-05 10:35:05 -0500

    updated alignment

--- home.php
+++ home.php
@@ -80,11 +80,10 @@
 
 <body>
 <div>
-<h1> 2AFC Duke RAILabs </h1> <h4 style="text-align:right; color:#737CA1;"> <a href="logout.php?logout">Sign Out</a> </h4>
+<h1> 2AFC Duke RAILabs </h1>
 </div>
 <!-- This is an example webpage to be utilized for 2AFC observer studies. -->
 
-<br>
 <br>
 
 <!-- setting up the html division to be container for cornstone enabled elements  -->
@@ -190,7 +189,7 @@
   <input id="zp" class="btn3" type="checkbox" checked="" style="margin-left: 1%;"> Synchronize Zoom/Pan
   <input id="pos" class="btn3" type="checkbox" checked="" style="margin-left: 1%;"> Synchronize Slice
 
-  <br>
+  <p style="text-align:right; color:#737CA1;"> <a href="logout.php?logout">Sign Out</a> </p>
   <br>
 
   <div id="myProgress">
