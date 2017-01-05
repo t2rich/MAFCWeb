59ebf43ca16d2ab96396fb243846d8cff03e2b8b
Taylor Richards <taylor.richards@duke.edu>
2017-01-05 17:33:56 -0500

    crazy styling

--- register.php
+++ register.php
@@ -123,13 +123,13 @@
     unset($utype);
     $res2=mysql_query("SELECT userId FROM users WHERE email='$email'");
     if ($res2){
-      // $row=mysql_fetch_array($res2);
-      // $query2 = "CREATE TABLE '$row['userId']' (study_index INT(3) UNSIGNED PRIMARY KEY, fnl VARCHAR(30) NOT NULL, fnr VARCHAR(30) NOT NULL, slices INT(3) UNSIGNED)";
-      // $res3 = mysql_query($query2);
-      // if (!$res3){
-      //   $errTyp = "danger";
-      //   $errMSG = "Something went wrong with database, try again later...";
-      // }else {
+      $row=mysql_fetch_array($res2);
+      $query2 = "CREATE TABLE '$row['userId']' (study_index INT(3) UNSIGNED PRIMARY KEY, fnl VARCHAR(30) NOT NULL, fnr VARCHAR(30) NOT NULL, slices INT(3) UNSIGNED)";
+      $res3 = mysql_query($query2);
+      if (!$res3){
+        $errTyp = "danger";
+        $errMSG = "Something went wrong with database, try again later...";
+      }//else {
       //   $errTyp = "success";
       //   $errMSG = echo $row['userId'];
       // }
