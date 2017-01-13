52962d710b89577a07dde46ad8d956535bf73dc5
Taylor Richards <taylor.richards@duke.edu>
2017-01-12 20:02:49 -0500

    Tons of backend expansion work

--- db/selection.php
+++ db/selection.php
@@ -23,36 +23,36 @@ $query = "INSERT INTO `".$userId."` (case_num, left_small, choice, small_dir, la
 
 $pre_res = mysql_query($query);
 
-if($pre_res){
-  $studyIndex = $studyIndex + 1;
-  $res=mysql_query("INSERT INTO users WHERE userId='$userId' (studyIndex) VALUES ($studyIndex)");
-  if($res){
-    $res2=mysql_query("SELECT * FROM `".$userId."` WHERE studyIndex='$studyIndex'");
-  if($res2){
-    $userRow2=mysql_fetch_array($res2);
-    $init_case_num = $userRow2['case_num'];
-
-    // load study info database
-    $res3=mysql_query("SELECT * FROM study_info WHERE case_num='$init_case_num'");
-    if($res3){
-      $userRow3=mysql_fetch_array($res3);
-      // get folder names, number of slices
-      $small_dir = $userRow3['small_dir'];
-      $large_dir = $userRow3['large_dir'];
-      $slices = $userRow3['slices'];
-      $left_small = rand(0,1);
-
-      if ($left_small) {
-        $left_side = $small_dir;
-        $right_side = $large_dir;
-      } else {
-        $left_side = $large_dir;
-        $right_side = $large_dir;
-      }
-      echo json_encode(array("left_side"=>$left_side,"right_side"=>$right_side,"left_small"=>$left_small,"slices"=>$slices))
-    }
-  }
-}
-
+// if($pre_res){
+//   $studyIndex = $studyIndex + 1;
+//   $res=mysql_query("INSERT INTO users WHERE userId='$userId' (studyIndex) VALUES ($studyIndex)");
+//   if($res){
+//     $res2=mysql_query("SELECT * FROM `".$userId."` WHERE studyIndex='$studyIndex'");
+//   if($res2){
+//     $userRow2=mysql_fetch_array($res2);
+//     $init_case_num = $userRow2['case_num'];
+//
+//     // load study info database
+//     $res3=mysql_query("SELECT * FROM study_info WHERE case_num='$init_case_num'");
+//     if($res3){
+//       $userRow3=mysql_fetch_array($res3);
+//       // get folder names, number of slices
+//       $small_dir = $userRow3['small_dir'];
+//       $large_dir = $userRow3['large_dir'];
+//       $slices = $userRow3['slices'];
+//       $left_small = rand(0,1);
+//
+//       if ($left_small) {
+//         $left_side = $small_dir;
+//         $right_side = $large_dir;
+//       } else {
+//         $left_side = $large_dir;
+//         $right_side = $large_dir;
+//       }
+//       echo json_encode(array("left_side"=>$left_side,"right_side"=>$right_side,"left_small"=>$left_small,"slices"=>$slices))
+//     }
+//   }
+// }
+echo $pre_res
 mysql_close($con);
 ?>
